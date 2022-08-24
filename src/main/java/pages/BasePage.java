12c23e91bf8b986d113acbcc88d478adb81acf17
ChrisASan <chris.sanchez1905@gmail.com>
2022-08-23 19:26:24 -0500

    first commit

---
+++ src/main/java/pages/BasePage.java
@@ -0,0 +1,30 @@
+
+package pages;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasePage {
+	 public static WebDriver driver;
+		
+	public static void init() {
+		System.setProperty("webdriver.chrome.driver", "driver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+	}
+	
+	public static void validation() {
+		
+	}
+	
+	public static int randNum() {
+		Random rand = new Random();
+		int randNum = rand.nextInt(999);
+		return randNum;
+	}
+}
